1e952b47adf1b594c033c402dd905953f71ee23a
Micky373 <michaeltamirie@gmail.com>
2023-04-07 17:21:54 +0300

    Edit the exception.py file

--- src/exception.py
+++ src/exception.py
@@ -4,10 +4,12 @@ from src.logger import logging
 
 # Creating a function that gives us the error message
 def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-     file_name,exc_tb.tb_lineno,str(error))
+    _,_,exc_tb=error_detail.exc_info() # Getting the error info
+    file_name=exc_tb.tb_frame.f_code.co_filename # Getting the error name
+
+    # Getting our custom error message
+    error_message=f"Error occured \nin python script:[{file_name}] \
+        \nline number:[{exc_tb.tb_lineno}] \nerror message:[{str(error)}]"
 
     return error_message
 
