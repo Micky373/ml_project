5d9bbe23c0ff01d2cb96c3db00fac79eed1ee1ae
Micky373 <michaeltamirie@gmail.com>
2023-04-07 17:13:49 +0300

    Add exception

---
+++ src/exception.py
@@ -0,0 +1,21 @@
+# Importing useful libraries and modules
+import sys
+from src.logger import logging
+
+# Creating a function that gives us the error message
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+     file_name,exc_tb.tb_lineno,str(error))
+
+    return error_message
+
+# Creating a class that will enable us to raise a custom exception and get the message
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
